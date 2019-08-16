1c975eabbea5c04ca9b6fd239b5d8041d0f82138
Michael Agard <mdagard@gmail.com>
2019-08-16 13:00:10 -0400

    Add unpublish, publish, and published? method

--- publishable/lib/publishable.rb
+++ publishable/lib/publishable.rb
@@ -2,5 +2,16 @@ require "publishable/version"
 
 module Publishable
   class Error < StandardError; end
-  # Your code goes here...
+  def publish
+    update.call(published_At) #figure this out eventually
+  end
+  
+  def unpublish
+    this.published_At = nil
+  end
+  
+  def published?
+    published_at.nil? ? false : true
+  end
+
 end
